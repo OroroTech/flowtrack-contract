362d10787ac4662fcc29f2b508d2b3e9ab4941b6
Isaac Ochuko <ochukoisaac660@gmail.com>
2026-07-15 11:00:46 +0000

    feat: define ContractError enum

--- src/errors.rs
+++ src/errors.rs
@@ -1,1 +1,12 @@
-// ContractError enum.
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum ContractError {
+    AlreadyEnrolled = 1,
+    RecipientNotFound = 2,
+    InvalidAmount = 3,
+    Unauthorized = 4,
+    ScheduleNotFound = 5,
+}
